3347c82208e8ea3a9bc45aa69011c06260268128
LinBii <aa68968@gmail.com>
2022-01-12 23:07:53 +0800

    feat: add method override

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -16,6 +17,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(port, () => {
