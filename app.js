ad65e2543a6f5bfb9c7ee6d06382993bfe0a5b02
LinBii <aa68968@gmail.com>
2022-01-12 22:21:16 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
@@ -13,9 +14,7 @@ const port = process.env.PORT
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
