ad715cb8efc395609b12080a460bb027cf1ac852
LinBii <aa68968@gmail.com>
2022-01-13 10:22:04 +0800

    feat: add edit record function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -14,4 +14,29 @@ router.get('/new', (req, res) => {
   return res.render('new')
 })
 
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((record) => {
+      record.date = record.date.toISOString().split('T')[0]
+      res.render('edit', { record })
+    })
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, date, amount } = req.body
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => res.redirect(`/`))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
