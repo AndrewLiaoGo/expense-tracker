d5304d691d876e8f59c374700f3bcaeea8d2d5ec
LinBii <aa68968@gmail.com>
2022-02-05 12:50:20 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,6 +15,7 @@ router.post('/login', passport.authenticate('local', {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
@@ -24,9 +25,25 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: '必需填寫email及密碼。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('此Email已被使用！')
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       res.render('register', {
         name,
         email,
