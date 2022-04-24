fc7204abcbfc181a7fd455fe8275801d00c1edc8
LinBii <aa68968@gmail.com>
2022-04-24 18:09:50 +0800

    feat: add category schema

--- models/record.js
+++ models/record.js
@@ -18,6 +18,12 @@ const recordSchema = new Schema({
     ref: 'User',
     index: true,
     required: true
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    index: true,
+    required: true
   }
 })
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,6 +4,14 @@ const router = express.Router()
 const Category = require('../../models/category')
 const Record = require('../../models/record')
 
+const CATEGORY = {
+  家居物業: "https://fontawesome.com/icons/home?style=solid",
+  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
+  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
+  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
+  其他: "https://fontawesome.com/icons/pen?style=solid"
+}
+
 router.get('/', async (req, res) => {
   const userId = req.user._id
   const categories = await Category.find({})
